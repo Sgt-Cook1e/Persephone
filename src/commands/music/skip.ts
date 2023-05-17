90fcea0f215c40b92f2da9424bb74fe3631c468a
MythicXGN <hydythediny@gmail.com>
2023-05-17 13:52:34 -0400

    NSFW & Music Updates

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -57,7 +57,7 @@ export default class SkipCommand extends Command {
                             ]
                         }
                     ]
-                })
+                });
             }
         }
     }
---
+++ src/commands/nsfw/hentai.ts
@@ -0,0 +1,78 @@
+import { Kore } from "../../structs/Kore";
+import { Command } from "../../structs/command";
+import { CommandInteraction, AnyTextChannelWithoutGroup, Uncached, ApplicationCommandTypes, TextChannel } from "oceanic.js";
+
+import { NightAPI } from "night-api";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+export default class HentaiCommand extends Command {
+    constructor(client: Kore) {
+        super(client, {
+            name: "nhentai",
+            description: "NSFW Image",
+            group: "NSFW",
+            slash: {
+                enabled: true,
+                type: ApplicationCommandTypes.CHAT_INPUT
+            }
+        });
+    }
+
+    public async interactionRun(interaction: CommandInteraction<AnyTextChannelWithoutGroup | Uncached>): Promise<void> {
+        const api = new NightAPI(`${process.env.NIGHTAPI}`);
+        const image = await api.nsfw.fetchImage("hentai");
+
+        if(this.client.getChannel<TextChannel>(interaction.channelID)?.nsfw === true){
+            if(await image){
+                interaction.createMessage({
+                    embeds: [
+                        {
+                            author: {
+                                name: `Hentai`,
+                                iconURL: interaction.user.avatarURL('jpeg')
+                            },
+                            
+                            image: {
+                                url: await image.content.url
+                            }
+                        }
+                    ]
+                });
+            } else {
+                interaction.createMessage({
+                    embeds: [
+                        {
+                            author: {
+                                name: `Api Broke Please Try again. or Try again Later`,
+                                iconURL: interaction.user.avatarURL('jpeg')
+                            }
+                        }
+                    ]
+                })
+            }
+        } else {
+            interaction.createMessage({
+                embeds: [
+                    {
+                        author: {
+                            name: `${interaction.guild?.name}, Tried to get a NSFW Command Outside of a nsfw channel`,
+                            iconURL: interaction.user.avatarURL('jpeg')
+                        },
+
+                        fields: [
+                            {
+                                name: `${interaction.user.username}`,
+                                value: `Please Use the Command In a NSFW Channel`
+                            }
+                        ],
+
+                        footer: {
+                            text: 'Created With Love by Mythic'
+                        }
+                    }
+                ]
+            });
+        }
+    }
+}
